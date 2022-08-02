4fc510bae465407d969d8a2fb8c37dd37678843d
chenhuicheng <chenhuicheng@bytedance.com>
2022-08-02 21:27:25 +0800

    feat: pull request
    Change-Id: I4eb27b0740ac9704d68596fc38efa041dd28de2a

--- Mygameproject/main.py
+++ Mygameproject/main.py
@@ -85,6 +85,8 @@ for i in range(st.enemy_num[0]): #创建敌人
 然后以文档路径的方式导进来即可
 
 或在设置文字font时把None(默认)改成中文'simsunnsimsun'
+
+我想 pull request 一下
 """
 #三个界面
 bg1 = begin.Beginpage('gameproject\\images\\bg1.jpg',screen)  # 开始界面背景
